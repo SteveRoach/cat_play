371efb7cd7164d3b72bdf6f06b0216c09f83e14b
steve_roach@hotmail.com <steve_roach@hotmail.com>
2018-02-20 22:16:35 +1100

    Journal 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'steve_roach@hotmail.com', user_type: 'admin', password: 'fu6CAT9ck',
+	password_confirmation: 'fu6CAT9ck')
